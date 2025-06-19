8bd3a25d4b6f6a539e7c43ace24400a005d1c9cb
Subhamoy Sasmal <2023aspire117@gmail.com>
2025-06-20 02:43:35 +0530

    some logic updated

---
+++ school.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const jwtSecret  = '123456';
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb+srv://subhamsasmal396:hello@cluster0.rudguct.mongodb.net/myDatabase', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+}
+
+
+app.use(express.json());
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.post('/register' ,async (req,res)=>{
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    try {
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+      return res.status(400).send('Bad Request: Email already exists');
+    }
+
+    const user = new User({ name, email, password });
+    await user.save();
+    const token = jwt.sign({ name: name , email: email }, jwtSecret, { expiresIn: '1h' });
+
+    res.status(201).json({
+      message: `User ${name} registered successfully`,
+      token: token
+    });
+  } catch (err) {
+    res.status(500).send(`Error: ${err.message}`);
+  }
+    
+})
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
